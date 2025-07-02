d5b2c1637af05cd611675d732385d3691a318a09
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-02 12:30:36 -0300

    feat: Navegando com useNavigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,19 @@
 import { ButtonIcon } from "@/components/ButtonIcon";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
+import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 
 export function Home() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <Title>Home</Title>
-        <ButtonIcon name="add-circle" />
+        <ButtonIcon
+          name="add-circle"
+          onPress={() => navigation.navigate("product")}
+        />
       </Header>
     </View>
   );
