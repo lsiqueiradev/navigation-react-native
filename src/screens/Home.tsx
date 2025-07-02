a81d6b58447a455f3762c596b363d0e212bf9b26
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-02 13:16:59 -0300

    feat: toggleDrawer

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -10,11 +10,13 @@ export function Home({ navigation }: DrawerRoutesProps<"home">) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <Title>Home</Title>
         <ButtonIcon
           name="add-circle"
           onPress={() => navigation.navigate("product", { id: "31" })}
         />
+        <Title>Home</Title>
+
+        <ButtonIcon name="menu" onPress={() => navigation.toggleDrawer()} />
       </Header>
     </View>
   );
