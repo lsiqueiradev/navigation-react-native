d0a1189b54ba8d0900520d1bd3afcee3229c998b
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-02 13:07:52 -0300

    feat: Customizando Bottom Navigator

--- src/routes/TabsRoutes.tsx
+++ src/routes/TabsRoutes.tsx
@@ -3,6 +3,8 @@ import {
   createBottomTabNavigator,
 } from "@react-navigation/bottom-tabs";
 
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { Home } from "@/screens/Home";
 import { Product } from "@/screens/Product";
 
@@ -18,9 +20,34 @@ const Tab = createBottomTabNavigator<TabRoutesList>();
 
 export function TabsRoutes() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="home" component={Home} />
-      <Tab.Screen name="product" component={Product} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#2C46B1",
+        tabBarInactiveTintColor: "#444444",
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
+      <Tab.Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="product"
+        component={Product}
+        options={{
+          tabBarLabel: "Produto",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="add-circle" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
