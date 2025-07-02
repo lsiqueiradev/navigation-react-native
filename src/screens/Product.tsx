fcb4c744114beb62945c2d679f9f96a34c0ddbd3
Lucas Siqueira <lucas@lsiqueira.dev>
2025-07-02 12:57:40 -0300

    feat: useRoute

--- src/screens/Product.tsx
+++ src/screens/Product.tsx
@@ -2,9 +2,13 @@ import { ButtonIcon } from "@/components/ButtonIcon";
 import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 import { StackRoutesProps } from "@/routes/StackRoutes";
+import { useRoute } from "@react-navigation/native";
 import { View } from "react-native";
 
+type RouteParams = StackRoutesProps<"product">;
+
 export function Product({ navigation, route }: StackRoutesProps<"product">) {
+  const { params } = useRoute<RouteParams["route"]>();
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
@@ -12,7 +16,8 @@ export function Product({ navigation, route }: StackRoutesProps<"product">) {
           name="arrow-circle-left"
           onPress={() => navigation.goBack()}
         />
-        <Title>Product {route.params?.id}</Title>
+        <Title>Product {params?.id}</Title>
+        {/* <Title>Product {route.params?.id}</Title> */}
       </Header>
     </View>
   );
